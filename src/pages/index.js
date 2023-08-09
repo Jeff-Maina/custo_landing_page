79ae32b212436c392926807061e8bc7a35ad74c3
Jeff-Maina <devyoungblud@gmail.com>
2023-08-09 17:59:38 +0300

    Added image scale  and text reveal animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
           animate={{ y: "-100%" }}
           transition={{
             y: {
-              duration: 1,
+              duration: 1.5,
               delay: 0.3,
               ease: [0.58, 0.13, 0, 1.01],
             },
@@ -27,7 +27,7 @@ export default function Home() {
               animate={{ y: "-100%" }}
               transition={{
                 y: {
-                  duration: 0.3,
+                  duration: 0.5,
                   delay: 0.3,
                 },
               }}
@@ -37,7 +37,7 @@ export default function Home() {
             </motion.p>
           </div>
         </motion.section>
-        <section className="w-full h-screen bg-[#9ea29f] xl:px-20 md:px-16 px-8 flex-col flex justify-between relative overflow-hidden">
+        <section className="w-full h-screen bg-[#9ea29f] xl:px-20 md:px-16 px-6 flex-col flex justify-between relative overflow-hidden">
           {/* <img src="" alt="" className="absolute w-screen h-screen object-cover top-0 left-0" /> */}
           <div>
             <Navbar />
@@ -91,22 +91,53 @@ export default function Home() {
             </div>
           </div>
 
-          <div className="w-full flex items-start justify-between relative z-10">
-            {Array.from("CUSTO").map((letter, index) => {
-              return (
-                <span className="text-white xl:text-[28rem] md:text-[15rem] md:leading-[30rem] text-[8rem] leading-[8rem] font-bold">
-                  {letter}
-                </span>
-              );
-            })}
-
-            <span className="text-white lg:text-[15rem] lg:leading-[30rem] text-[3rem] font-normal -translate-y-1/4">
-              ®
-            </span>
+          <div className="w-full flex items-end relative z-10 lg:pb-4 pb-8">
+            <div className="flex items-center w-full justify-evenly  lg:h-[24rem] overflow-hidden ">
+              {Array.from("CUSTO").map((letter, index) => {
+                return (
+                  <motion.div
+                    initial={{ y: "100%" }}
+                    animate={{ y: 0 }}
+                    transition={{
+                      y: {
+                        duration: 1,
+                        delay: 0.1 * index + 0.3,
+                        ease: [0.25, 0.1, 0.25, 1],
+                      },
+                    }}
+                    className="text-white xl:text-[28rem] md:text-[15rem] md:leading-[30rem] text-[7rem] leading-[8rem] font-bold"
+                  >
+                    {letter}
+                  </motion.div>
+                );
+              })}{" "}
+              <motion.span
+                initial={{ y: "100%" }}
+                animate={{ y: "-20%" }}
+                transition={{
+                  y: {
+                    duration: 1,
+                    delay: 0.8,
+                    ease: [0.25, 0.1, 0.25, 1],
+                  },
+                }}
+                className="text-white lg:text-[15rem] lg:leading-[25rem] text-[3rem] font-normal -translate-y-1/4"
+              >
+                ®
+              </motion.span>
+            </div>
           </div>
 
           <div className="min-w-2/4 min-h-2/4  absolute left-2/4 bottom-0 -translate-x-2/4 grid place-items-center">
-            <img
+            <motion.img
+              initial={{ scale: 1.2 }}
+              animate={{ scale: 1, y: "62%"}}
+              transition={{
+                scale: {
+                  duration: 1.3,
+                  ease: [0.25, 0.1, 0.25, 1],
+                },
+              }}
               src="https://custo.io/cdn/shop/files/custo-hero_592x1234_crop_center.png?v=1669139239"
               alt=""
               className="lg:translate-y-[62%] translate-y-2/4 lg:w-[80vmin]"
